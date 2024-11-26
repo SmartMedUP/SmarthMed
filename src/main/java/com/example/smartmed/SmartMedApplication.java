b9f12a47fe01a492f00eef5d6c36215c12188aad
Ahmad ISSAWI <ahmadissawi360@gmail.com>
2024-11-26 20:11:00 +0100

    initialisation du projet (creation base de donnée)

---
+++ src/main/java/com/example/smartmed/SmartMedApplication.java
@@ -0,0 +1,14 @@
+package com.example.smartmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMedApplication.class, args);
+        //System.out.println("salem");
+    }
+
+}
